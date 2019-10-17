e996ff4099a670b3da0eee4c7e78dde13770c163
Robin <swesson92@gmail.com>
2019-10-17 16:57:34 +0200

    testing jwt

---
+++ src/main/java/robin/springsite/robins/RobinsApplication.java
@@ -0,0 +1,13 @@
+package robin.springsite.robins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/robin/springsite/robins/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package robin.springsite.robins.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping({ "/hello" })
+    public String firstPage() {
+        return "Hello World";
+    }
+
+}
